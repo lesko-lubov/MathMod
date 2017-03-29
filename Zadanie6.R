e7ac638d91ba0e2205769a5813c672ee32e0dccf
lesko-lubov <leskolv94@gmail.com>
2017-03-29 17:10:50 +0300

    Задание 6

---
+++ Zadanie6.R
@@ -0,0 +1,11 @@
+median <- function(x) { 
+  z=sort(x) 
+  if((length(z)%%2)!=0){ 
+    result = z[(length(x)/2)+1] 
+  } 
+  else 
+    result = (z[length(x)/2]+z[length(x)/2+1])/2 
+  return(result) 
+} 
+median(iris$Sepal.Length)
+  
